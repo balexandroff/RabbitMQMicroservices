e721b3d447e547ca894dc4dd4db00057b219cf91
balexandrov <balexandrov@topviewnyc.com>
2021-10-04 15:35:43 +0300

    Implemented basic Dependency injection of RabbitMQBus

---
+++ RabbitMQMicroservices/rabbitMQMicroservices.Infrastructure.Dependency/DependencyContainer.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using RabbitMQMicroservices.Domain.Core.Bus;
+using RabbitMQMicroservices.Infrastructure.Bus;
+
+namespace RabbitMQMicroservices.Infrastructure.Dependency
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            //Domain Bus
+            services.AddTransient<IEventBus, RabbitMQBus>();
+        }
+    }
+}
